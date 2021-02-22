6ef805cf209283eb8ec55991ac831879db00cc64
AndrewFrazerMP <Andrew.Frazer@malvernpanalytical.com>
2021-02-22 16:00:38 +0000

    Initial commit

---
+++ SharedSource-ClassLibrary/SharedClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedSource_ClassLibrary
+{
+    public class Class
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+    }
+}
